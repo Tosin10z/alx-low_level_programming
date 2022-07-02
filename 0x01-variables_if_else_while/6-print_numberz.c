9b60c7480363d159af6b0896f28cffc8b450a0c6
Tosin10z <tossynnwahab11@gmail.com>
2022-07-02 11:55:49 -0700

    TASK 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; n++)
+		putchar(ch);
+			putchar(10);
+			return (0);
+}
+
