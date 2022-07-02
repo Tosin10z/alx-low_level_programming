00091740e0d7ed664ec39877f9a0e55d27e02342
Tosin10z <tossynnwahab11@gmail.com>
2022-07-02 12:39:09 -0700

    TASK 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-	int ch;
+	int n;
+	char ch;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
 
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
+
 	putchar(10);
+
 	return (0);
 }
