d7bdbf3dc8abed5404f22f1bbc9aa472d6b4d534
Tosin10z <tossynnwahab11@gmail.com>
2022-07-04 16:16:25 -0700

    TASK 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,6 +10,7 @@ int main(void)
 	int ch, n, x;
 
 		for (ch = 48; ch <= 57; ch++)
+		{
 		for (n = 49; n <= 57; n++)
 		for (x = 50; x <= 57; x++)
 			if (x > n && n > ch)
@@ -23,6 +24,7 @@ int main(void)
 				putchar(44);
 				putchar(32);
 			}
+		}
 			putchar(10);
 		return (0);
 }
