4723fbf12d5aa85c9bdf4872307341034459b900
Tosin10z <tossynnwahab11@gmail.com>
2022-07-07 18:22:15 -0700

    TASK 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straght line
+ * @n: The number of lines to draw
+ *
+ * Return: the line
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+}
