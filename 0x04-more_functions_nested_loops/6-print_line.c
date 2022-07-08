e709785c33715bf63120415955a9ed9156439604
Tosin10z <tossynnwahab11@gmail.com>
2022-07-08 14:27:07 -0700

    TASK 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,16 @@ void print_line(int n)
 {
 	int x;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar ('\n');
 	}
 	else
-		for (x = 0; x <= n; x++)
-		{
-			_putchar('_');
-		}
-	_putchar('\n');
+	{
+			_putchar('\n');
+	}
 }
