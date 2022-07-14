8d651709fcf5f990e4dc8cb95c075840a552001a
Tosin10z <tossynnwahab11@gmail.com>
2022-07-14 14:10:05 -0700

    TASK 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: string pointer
+ * @src: string pointer
+ * @n: int parameter
+ *
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
