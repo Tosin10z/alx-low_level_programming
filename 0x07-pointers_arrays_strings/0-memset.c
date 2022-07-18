73da86b823d45e4307757d446756ff65968fbe41
Tosin10z <tossynnwahab11@gmail.com>
2022-07-18 12:01:25 -0700

    TASK 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: int pointer
+ * @b: int parameter
+ * @n: int parameter
+ *
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index;
+	char *memory = s, value = b;
+	
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
+}
